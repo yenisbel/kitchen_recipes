b17e025890778caca5466d6026f32cb50c116d6e
YeniValle <yenivalle@YeniValles-MBP.attlocal.net>
2021-10-19 17:09:16 -0400

    Add some style and img for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,8 @@ class RecipesController < ApplicationController
 
   # GET /recipes or /recipes.json
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.all.order(created_at: :desc)
+    
   end
 
   # GET /recipes/1 or /recipes/1.json
